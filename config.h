90d3b63719474cb644c10650e1c2f4c6aa86ae76
KubaBoi <jakubanderle@outlook.cz>
2023-02-16 20:00:31 +0100

    ok

--- config.h
+++
@@ -1,9 +0,0 @@
-
-#define LILYGO_WATCH_2020_V3              // Use T-Watch2020
-// #define LILYGO_WATCH_LVGL
-
-#define SHADE_TIME 20 // in seconds
-#define SCREEN_TIME 30 // in seconds
-#define SLEEP_TIME 60 // in seconds
-
-#include <LilyGoWatch.h>
