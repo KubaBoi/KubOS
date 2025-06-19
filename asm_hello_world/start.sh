2418ec15cafaa0daffb1c54e2989b86751d4ab7e
KubaBoi <jakubanderle@outlook.cz>
2025-06-19 23:20:26 +0200

    branch restart

--- asm_hello_world/start.sh
+++
@@ -1,9 +0,0 @@
-#as -g -o main.o main.S
-#as -g -o print.o print.S
-#gcc -c main.S -o main.o
-nasm "main copy.S" -o main.o -f elf64
-ld --oformat binary -o main.img -T link.ld main.o
-qemu-system-x86_64 -hda main.img -D logfile #-s -S
-
-#objdump -b binary -mi386 -D main.img
-
--- asm_hello_world/startgdb.sh
+++
@@ -1,2 +0,0 @@
-gdb main.o -tui -s main.S
-#target remote localhost:1234
